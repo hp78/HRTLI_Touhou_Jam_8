e370e0ed7bfef1c808e181075a9382c0236d2a9f
Hilal Fitri <hilal.f@nus.edu.sg>
2022-04-18 22:07:41 +0800

    Whoops cutscene timescale fix

--- Assets/Scripts/CutscenSceneChanger.cs
+++ Assets/Scripts/CutscenSceneChanger.cs
@@ -4,6 +4,11 @@ using UnityEngine.SceneManagement;
 public class CutscenSceneChanger : MonoBehaviour
 {
     public string levelToLoad;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
     public void ChangeLevel()
     {
         SceneManager.LoadScene(levelToLoad);
