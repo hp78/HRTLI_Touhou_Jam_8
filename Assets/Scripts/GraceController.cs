a93a7018b8541356f1b4536c0857892509363970
Hilal Fitri <hilal.f@nus.edu.sg>
2022-04-18 23:17:16 +0800

    UI stuff

---
+++ Assets/Scripts/GraceController.cs
@@ -0,0 +1,34 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GraceController : MonoBehaviour
+{
+    public string levelToLoad;
+    float currHeld = 0.0f;
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+    void ChangeLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currHeld > 2.0f)
+        {
+            ChangeLevel();
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            currHeld += Time.deltaTime;
+        }
+        else
+        {
+            currHeld = 0;
+        }
+    }
+}
